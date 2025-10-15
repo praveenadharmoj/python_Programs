4f4f9a3107da5abcf37f06bf57a73e135b9a0410
shiva <shiva.dharmoj@gmail.com>
2025-10-15 12:17:56 +0530

    fw_KW

---
+++ src/fw_Kw.py
@@ -0,0 +1,8 @@
+list = ['apples','mangoes','eggs','milk','coffee']
+#print(list)
+for items in list :
+  #  print(items)
+    index = 0 
+    while index < len(list) :
+        print(list[index])
+        index+=1
